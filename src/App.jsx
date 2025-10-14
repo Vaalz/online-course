569bc36650eb70c0f3fb58bbefa6f7b5c80aa5e2
ryan <abdiryan12@gmail.com>
2025-10-14 13:53:28 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function App() {
   return (
-    <Typography>tes 1</Typography>
+    <Typography>tes 1</Typography>,
+    <Typography>tes 2</Typography>,
+    <Typography>tes 3</Typography>
   )
 }
 
